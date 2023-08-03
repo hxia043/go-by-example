9434d291bc170b21ddc0750cb0c6422d3c9292ee
Xia Huyun <huyun.xia@nokia-sbell.com>
2023-08-04 00:07:43 +0800

    Add apiserver as a go-restful example

--- go-restful/main.go
+++ go-restful/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"io"
-	"log"
 	"net/http"
 
-	"github.com/emicklei/go-restful"
+	"github.com/emicklei/go-restful/v3"
+	"k8s.io/apiserver/pkg/server"
+	"k8s.io/kubernetes/pkg/api/legacyscheme"
 )
 
 func hello(req *restful.Request, resp *restful.Response) {
@@ -13,9 +14,31 @@ func hello(req *restful.Request, resp *restful.Response) {
 }
 
 func main() {
-	ws := new(restful.WebService)
-	ws.Route(ws.GET("/hello").To(hello))
-	restful.Add(ws)
+	/*
+		ws := new(restful.WebService)
+		ws.Route(ws.GET("/hello").To(hello))
+		restful.Add(ws)
 
-	log.Fatal(http.ListenAndServe(":8083", nil))
+		log.Fatal(http.ListenAndServe(":8083", nil))
+	*/
+	handler := server.NewAPIServerHandler(
+		"test-server",
+		legacyscheme.Codecs,
+		func(apiHandler http.Handler) http.Handler {
+			return apiHandler
+		},
+		nil)
+
+	testApisV1 := new(restful.WebService).Path("/apis/test/v1")
+	{
+		testApisV1.Route(testApisV1.GET("hello").To(
+			func(req *restful.Request, resp *restful.Response) {
+				resp.WriteAsJson(map[string]interface{}{"k": "v"})
+			},
+		)).Doc("hello endpoint")
+	}
+
+	handler.GoRestfulContainer.Add(testApisV1)
+
+	panic(http.ListenAndServe(":8080", handler))
 }
